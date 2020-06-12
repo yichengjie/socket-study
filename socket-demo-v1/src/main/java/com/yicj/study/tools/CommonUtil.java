8ea577c09ec06fc02d32dc4bdcd480d9b804ab74
易成杰 <626659321@qq.com>
2020-06-12 22:55:46 +0800

    bytebuffer使用

--- socket-demo-v1/src/main/java/com/yicj/study/tools/CommonUtil.java
+++ socket-demo-v1/src/main/java/com/yicj/study/tools/CommonUtil.java
@@ -1,5 +1,7 @@
 package com.yicj.study.tools;
 
+import java.nio.ByteBuffer;
+
 /**
  * ClassName: CommonUtil
  * Description: TODO(描述)
@@ -11,30 +13,54 @@ package com.yicj.study.tools;
  */
 public class CommonUtil {
 
+
     public static void main(String[] args) {
         int a = 1196 ;
-        byte[] bytes = int2ByteArray(a);
+        byte[] bytes = int2ByteArray2(a);
         int b = bytes2Int(bytes);
         System.out.println(b);
+
+        ByteBuffer buffer = ByteBuffer.wrap(bytes) ;
+        int anInt = buffer.getInt();
+        System.out.println(anInt);
+
+        new String() ;
     }
 
+    public static int bytes2Int(byte [] values){
+        return  (values[3] & 0xFF) |
+                ((values[2] & 0xFF) << 8)  |
+                ((values[1] & 0xFF) << 16) |
+                ((values[0] & 0xFF) << 24)  ;
+    }
 
-    public static int bytes2Int(byte[] bytes){
-        int num= ((bytes[0] <<24) & 0xFF0000) |
-                 ((bytes[1] <<16) & 0xFF0000) |
-                 ((bytes[2] <<8) & 0xFF00) |
-                 (bytes[3] & 0xFF) ;
+    public static int bytes2Int2(byte[] bytes){
+        int num=bytes[3] & 0xFF;
+        num |=((bytes[2] <<8) & 0xFF00);
+        num |=((bytes[1] <<16) & 0xFF0000);
+        num |=((bytes[0] <<24) & 0xFF0000);
         return num;
     }
 
 
+    public static int bytes2Int3(byte[] bytes){
+       ByteBuffer buffer = ByteBuffer.wrap(bytes) ;
+       return buffer.getInt() ;
+    }
+
     public static byte [] int2ByteArray(int value){
         return new byte[]{
-            (byte)((value >> 24)),
-            (byte)((value >> 16)),
-            (byte)((value >> 8)) ,
-            (byte)(value)
+            (byte)((value >> 24) & 0xFF),
+            (byte)((value >> 16) & 0xFF),
+            (byte)((value >> 8) & 0xFF) ,
+            (byte)(value & 0xFF)
         } ;
+    }
+
+    public static byte [] int2ByteArray2(int value){
+       ByteBuffer buffer = ByteBuffer.allocate(4) ;
+       buffer.putInt(value) ;
+       return buffer.array() ;
     }
 
 }
