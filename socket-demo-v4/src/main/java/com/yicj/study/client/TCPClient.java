f9e55a0ecc3e2297f2a29d19279f424468a227a0
易成杰 <626659321@qq.com>
2020-06-14 14:37:54 +0800

    udp整合tcp

---
+++ socket-demo-v4/src/main/java/com/yicj/study/client/TCPClient.java
@@ -0,0 +1,13 @@
+package com.yicj.study.client;
+
+/**
+ * ClassName: TCPClient
+ * Description: TODO(描述)
+ * Date: 2020/6/14 14:23
+ *
+ * @author yicj(626659321 @ qq.com)
+ * 修改记录
+ * @version 产品版本信息 yyyy-mm-dd 姓名(邮箱) 修改信息
+ */
+public class TCPClient {
+}
---
+++ socket-demo-v4/src/main/java/com/yicj/study/server/TCPServer.java
@@ -0,0 +1,13 @@
+package com.yicj.study.server;
+
+/**
+ * ClassName: TCPServer
+ * Description: TODO(描述)
+ * Date: 2020/6/14 13:54
+ *
+ * @author yicj(626659321 @ qq.com)
+ * 修改记录
+ * @version 产品版本信息 yyyy-mm-dd 姓名(邮箱) 修改信息
+ */
+public class TCPServer {
+}
